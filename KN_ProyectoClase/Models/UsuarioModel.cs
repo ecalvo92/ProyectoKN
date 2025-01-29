45028052dd0bb1b0700bac0de9f6c524272b1b61
ecalvo92 <ecalvo@crnova.com>
2025-01-28 20:25:15 -0600

    UI de inicio de sesión

---
+++ KN_ProyectoClase/Models/UsuarioModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KN_ProyectoClase.Models
+{
+    public class UsuarioModel
+    {
+        public string Identificacion { get; set; }
+        public string Contrasenna { get; set; }
+    }
+}
